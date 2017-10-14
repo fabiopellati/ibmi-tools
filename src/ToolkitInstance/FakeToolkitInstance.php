e7008e711b79632e318b26527703880786752371
Fabio Pellati <fabio.pellati@gmail.com>
2017-10-14 11:09:36 +0200

    Add debugMetods to FakeInstance

--- src/ToolkitInstance/FakeToolkitInstance.php
+++ src/ToolkitInstance/FakeToolkitInstance.php
@@ -10,12 +10,56 @@
 
 namespace IbmiTools\ToolkitInstance;
 
-use IbmiTools\Exception\RuntimeException;
 use ToolkitApi\Toolkit;
 
 class FakeToolkitInstance
     extends Toolkit
 {
+    public static $fakeLog = [];
+
+    static function AddParameterChar($io, $size, $comment, $varName = '', $value = '', $varying = 'off', $dimension = 0,
+                                     $by = '', $isArray = false, $ccsidBefore = '', $ccsidAfter = '', $useHex = false)
+    {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+
+        return [__CLASS__, __METHOD__];
+    }
+
+    static function AddParameterPackDec($io, $length, $scale, $comment, $varName = '', $value = '', $dimension = 0)
+    {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+
+        return [__CLASS__, __METHOD__];
+    }
+
+    public function isError()
+    {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+
+        return false;
+    }
+
+    public function getErrorMsg()
+    {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+
+        return 'FakeToolkit ' . __METHOD__;
+    }
+
+    public function getErrorCode()
+    {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+
+        return 500;
+    }
+
+    public function ClCommandWithOutput($command)
+    {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+
+        return ['fakeToolkit ', __METHOD__];
+    }
+
     /**
      * if passing an existing resource and naming, don't need the other params.
      *
@@ -29,6 +73,7 @@ class FakeToolkitInstance
      */
     public function __construct($databaseNameOrResource, $userOrI5NamingFlag = '0', $password = '', $transportType = '', $isPersistent = false)
     {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
     }
 
     /**
@@ -43,6 +88,15 @@ class FakeToolkitInstance
      */
     public function ExecuteProgram($inputXml, $disconnect = false)
     {
-        throw new RuntimeException('si sta usando la versione Fake del toolkit, nessuna risposta prevista', 500);
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+
+        return ['fakeToolkit ', __METHOD__];
+
     }
+
+    public function CLCommand($command, $exec = '')
+    {
+        self::$fakeLog[] = [__METHOD__, func_get_args()];
+    }
+
 }
